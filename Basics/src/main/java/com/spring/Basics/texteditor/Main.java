19076890610bd55a4be292d409d3c76136abd398
GIT_NAME <GIT_EMAIL>
2020-07-22 19:19:51 +0300

    update

--- Basics/src/main/java/com/spring/Basics/texteditor/Main.java
+++ Basics/src/main/java/com/spring/Basics/texteditor/Main.java
@@ -1,25 +1,13 @@
 package com.spring.Basics.texteditor;
 
 import com.spring.Basics.texteditor.config.TextEditorConfig;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import static org.junit.Assert.*;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = TextEditorConfig.class)
 public class Main {
-
-    @Autowired
-    private TextEditor textEditor;
-
-    @Test
-    public void testEditor() {
-        assertNotNull(textEditor);
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(TextEditorConfig.class);
+        TextEditor textEditor = applicationContext.getBean(TextEditor.class);
         textEditor.spellCheck();
     }
 }
--- Basics/src/test/java/com/spring/Basics/texteditor/TextEditorTest.java
+++ Basics/src/test/java/com/spring/Basics/texteditor/TextEditorTest.java
@@ -1,7 +1,24 @@
 package com.spring.Basics.texteditor;
 
+import com.spring.Basics.texteditor.config.TextEditorConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.*;
 
-class TextEditorTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TextEditorConfig.class)
+public class TextEditorTest {
+    @Autowired
+    private TextEditor textEditor;
 
+    @Test
+    public void testEditor() {
+        assertNotNull(textEditor);
+        textEditor.spellCheck();
+    }
 }
